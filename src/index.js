387d7a043c2dfde9dbba59a9f0c5eb2e52e234c1
Varun Krishnan V <varunkrishnan0001@gmail.com>
2026-09-15 16:34:26 +0530

    dev tinder project started

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Node JS")
+})
+
+app.get("/about", (req, res) => {
+    res.send("About")
+})
+app.get("/about2", (req, res) => {
+    res.send("About2")
+})
+
+app.listen("5500", () => {
+    console.log("App is running");
+})
